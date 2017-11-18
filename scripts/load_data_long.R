520b412395001502acb89575861cf75810f8d871
Conor Tompkins <conorotompkins@gmail.com>
2017-11-18 15:11:36 +0000

    worked on df_wide

--- scripts/load_data_long.R
+++ scripts/load_data_long.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(lubridate)
+library(stringr)
 
 rm(list = ls())
 data_path <- "./data/overdoses"
@@ -37,8 +38,44 @@ data %>%
 rm("data")
 
 df_long %>% 
-  count(overdose_factor, sort = TRUE) -> od_factors
+  filter(overdose_factor != "Unknown") %>% 
+  count(overdose_factor, sort = TRUE) %>% 
+  top_n(10) -> od_factors
 od_factors
 
+df_long %>% 
+  count(overdose_number, sort = TRUE)
 
-df_wide 
+
+df_long %>% 
+  spread(overdose_number, overdose_factor) %>% 
+  #group_by(id, date, death_time, year, yday, month, mday, wday, ) %>% 
+  mutate(overdose_factors = str_c(combined_od1, 
+                            combined_od2, 
+                            combined_od3, 
+                            combined_od4, 
+                            combined_od5, 
+                            combined_od6, 
+                            combined_od7, sep = ", ")) %>% 
+  select(-c(contains("combined_od"))) %>% 
+  mutate(overdose_factors = str_replace_all(overdose_factors, "Unknown, ", ""),
+         overdose_factors = str_replace_all(overdose_factors, ", Unknown", ""),
+         od_heroin = str_detect(overdose_factors, "Heroin"),
+         od_cocaine = str_detect(overdose_factors, "Cocaine"),
+         od_fentanyl = str_detect(overdose_factors, "Fentanyl"),
+         od_alcohol = str_detect(overdose_factors, "Alcohol"))  %>% 
+  mutate(od_factor_count = rowSums(select(., contains("od_")))) -> df_wide
+ 
+
+df_wide %>%
+  select(starts_with("od_"))
+  
+?sum
+%>% 
+   -> df_wide
+        
+?str_replace
+?rowwise
+df_wide %>% 
+  filter(od_heroin == TRUE)
+?str_detect
