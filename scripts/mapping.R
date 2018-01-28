298588646d2602bef51c82b5274715f0b4867838
Conor Tompkins <conorotompkins@gmail.com>
2018-01-28 15:12:02 -0500

    cleaned up and commented code

--- scripts/mapping.R
+++ scripts/mapping.R
@@ -1,32 +1,45 @@
+#load packages
 library(tidyverse)
 library(tidycensus)
 library(sf)
 library(viridis)
 library(ggmap)
+library(zipcode)
 
+#load od data
 source("scripts/load_data.R")
+
+#set cache and api key
 options(tigris_use_cache = TRUE)
 Sys.getenv("CENSUS_API_KEY")
 
-df_zips <- df %>%
-  select(incident_zip) %>% 
-  distinct()
+#write_csv(df_zips, "data/zip_codes.csv")
 
-write_csv(df_zips, "data/zip_codes.csv")
+#load census variables
+v15 <- load_variables(2016, "acs5", cache = TRUE)
 
+View(v15)
+
+#load zip codes from OD data
 df_zips <- read_csv("data/zip_codes.csv")
 
+#load PA zip codes
+data("zipcode")
+pa_zips <- zipcode %>% 
+  filter(state == "PA")
+
+#query census api
 usa <- get_acs(geography = "zcta", 
-                     variables = "B01003_001",
+                     variables = c(total_population = "B01003_001E"),
                      geometry = TRUE) %>% 
-  mutate(NAME = str_replace(NAME, "ZCTA5 ", "")) %>% 
-  st_transform(crs = 3857)
+  mutate(NAME = str_replace(NAME, "ZCTA5 ", ""))
 
-allegheny <- df_zips %>%
-    left_join(usa, by = c("incident_zip" = "NAME"))
+#join PA zips with census df
+pennsylvania <- pa_zips %>%
+    left_join(usa, by = c("zip" = "NAME"))
 
-
-allegheny %>%
+#plot
+pennsylvania %>%
   ggplot(aes(fill = estimate, color = estimate)) + 
   geom_sf() + 
   coord_sf(crs = 26911) + 
@@ -34,10 +47,18 @@ allegheny %>%
   scale_color_viridis(option = "magma")
 
 
+
+
+
+
+
+
+
+#does not work
 allegheny_map <- get_map(location = "Allegheny County")
 ggmap(allegheny_map) +
-  geom_polygon(data = allegheny, aes(fill = estimate, color = estimate)) + 
-  coord_sf(crs = 26911) + 
+  geom_sf(data = allegheny, aes(fill = estimate, color = estimate), inherit.aes = FALSE) + 
+  #coord_sf(crs = 26911) + 
   scale_fill_viridis(option = "magma") + 
   scale_color_viridis(option = "magma")
 
