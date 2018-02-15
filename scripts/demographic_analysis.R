7809af9fe147b6d4d392e1cdbc0bb5068fff7139
Conor Tompkins <conorotompkins@gmail.com>
2018-02-15 01:09:05 +0000

    updated plots

--- scripts/demographic_analysis.R
+++ scripts/demographic_analysis.R
@@ -6,42 +6,95 @@ library(scales)
 library(viridis)
 library(ggrepel)
 library(scales)
+library(knitr)
+library(kableExtra)
 
 theme_set(theme_bw(base_size = 18))
 
+ct_caption = "Conor Tompkins, @conor_tompkins"
+ct_subtitle = "Data from the WPRDC"
+ct_title = "Demographics of overdoses in Allegheny County"
+
 df %>% 
-  select(id, date, year, age, sex, race) %>% 
-  filter(date <= "2017-09-01")-> df_demo
-df_demo
+  select(id, date, year, age, sex, race) %>%
+  filter(date <= "2017-09-01") %>% 
+  mutate(category = str_c(sex, race, sep = ", ")) -> df_demo
 
 df_demo %>% 
-  count(race, sort = TRUE)
+  head() %>% 
+  kable("html") %>% 
+  kable_styling()
+
+df_demo %>% 
+  count(race, sort = TRUE) %>% 
+  head() %>% 
+  kable("html") %>% 
+  kable_styling()
 
 df_demo %>% 
   filter(race %in% c("White", "Black")) %>% 
   ggplot(aes(age, fill = race)) +
-  geom_density(alpha = .75)
+  geom_density(alpha = .75) +
+  labs(title = ct_title,
+       subtitle = ct_subtitle,
+       x = "Age",
+       y = "",
+       caption = ct_caption) +
+  scale_fill_discrete("Race")
 
 df_demo %>% 
   ggplot(aes(age, fill = sex)) +
-  geom_density(alpha = .75)
+  geom_density(alpha = .75) +
+  labs(title = ct_title,
+       subtitle = ct_subtitle,
+       x = "Age",
+       y = "",
+       caption = ct_caption) +
+  scale_fill_discrete("Sex")
+
+df_demo %>% 
+  count(category, sort = TRUE) %>% 
+  head() %>% 
+  kable("html") %>% 
+  kable_styling()
 
 df_demo %>% 
   filter(race %in% c("White", "Black")) %>% 
-  mutate(category = str_c(sex, race, sep = ", ")) %>% 
   ggplot(aes(age, fill = category)) +
-  geom_density(alpha = .5)
+  geom_density(alpha = .5) +
+  facet_wrap(~category, nrow = 1) +
+  labs(title = ct_title,
+       subtitle = ct_subtitle,
+       x = "Age",
+       y = "",
+       caption = ct_caption) +
+  scale_fill_discrete("")
 
 df_demo %>% 
   filter(race %in% c("White", "Black")) %>% 
-  mutate(category = str_c(sex, race, sep = ", ")) %>% 
   arrange(category, date) %>% 
   count(category, date) %>% 
   group_by(category) %>% 
-  mutate(cum_sum = cumsum(n)) %>% 
-  ggplot(aes(date, cum_sum, color = category)) +
+  mutate(cum_sum = cumsum(n)) -> df_demo_cumulative
+
+df_demo %>% 
+  filter(race %in% c("White", "Black")) %>% 
+  group_by(category) %>% 
+  summarize(total = n(),
+            last_date = last(date)) -> df_demo_cumulative_tag
+
+ggplot(data = df_demo_cumulative, aes(x = date, y = cum_sum, color = category)) +
   geom_line(size = 2) +
-  scale_y_continuous(label = comma)
+  geom_point(data = df_demo_cumulative_tag, aes(x = last_date, y = total), size = 3) +
+  geom_label_repel(data = df_demo_cumulative_tag, aes(x = last_date, y = total, color = category, label = category),
+                   nudge_x = 3) +
+  scale_y_continuous(label = comma) +
+  scale_color_discrete(guide = FALSE) +
+  labs(title = ct_title,
+       subtitle = ct_subtitle,
+       x = "",
+       y = "Cumulative sum of overdoses",
+       caption = ct_caption)
 
 #drug factor by category (sex, race)
 df %>% 
@@ -84,5 +137,7 @@ ggplot(data = df_factors_cumsum, aes(x = date, y = od_cumsum, color = od_factor)
   facet_wrap(~category) +
   scale_color_discrete(guide = FALSE) +
   scale_y_continuous(expand = c(.01, .1), label = comma) +
-  labs(y = "Cumulative sum of fatal overdoses by overdose factor",
-       caption = "Not exclusive")
+  labs(title = ct_title,
+       subtitle = ct_subtitle,
+       y = "Cumulative sum of fatal overdoses",
+       caption = "Conor Tompkins, @conor_tompkins \nNot exclusive")
